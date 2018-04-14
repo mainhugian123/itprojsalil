19e1c0759d161719b5c757664a1853589ed425c0
salil aggarwal <salilaggarwal17@gmail.com>
2018-04-14 14:54:00 +0530

    database daal diya

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap 
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length, AnyOf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'DontTellAnyone'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/salil/flask/database.db'
 Bootstrap(app)
-
+db = SQLAlchemy(app)
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(), unique=True)
+	username = db.Column(db.String(20), unique=True)
+	email = db.Column(db.String())
+	password = db.Column(db.String(80))
 
 class LoginForm(Form):
 	username = StringField('Username', validators=[InputRequired(), Length(min=1, max=20)])
@@ -32,7 +40,10 @@ def login():
 def signup():
 	form = SignUpForm()
 	if form.validate_on_submit():
-		return "Form Successfully Submitted"
+		new_user = User(name = form.name.data,username = form.username.data, email = form.email.data, password = form.password.data)
+		db.session.add(new_user)
+		db.session.commit
+		return "New User Has been created."
 	return render_template('signup.html',form=form)
 if __name__=='__main__':
 	app.run(debug=True)
